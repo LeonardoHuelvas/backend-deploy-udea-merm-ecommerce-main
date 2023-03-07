f9f4b4c761e5ed4ec5058b63034b7e83137ea8a3
LeonardoHuelvas <lejhubo01@hotmail.com>
2023-03-07 00:22:49 -0500

    express.import

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,16 +1,16 @@
 import express from "express";
 import {
-  prueba,
-  registrar,
-  confirmar,
-  auntenticar,
-  perfil,
-  usuarioRegistrados,
-  comprobarToken,
-  nuevoPassword,
-  actualizarPerfil,
-  olvidePassword,
-  actualizarPassword,
+    prueba,
+    registrar,
+    confirmar,
+    auntenticar,
+    perfil,
+    usuarioRegistrados,
+    comprobarToken,
+    nuevoPassword,
+    actualizarPerfil,
+    olvidePassword,
+    actualizarPassword,
 } from "../controllers/usuarioController.js";
 // middleware para validar el token
 import checkAuth from "../middleware/authMiddleware.js";
@@ -26,7 +26,7 @@ router.post("/olvide-password", olvidePassword);
 router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
 // Rutas Protegidas atraves del middleware checkAuth
 // Identificamos el usuario y se identifica para mostrale los datos o funcionalidades que le corresponden.
-router.get('/perfil', checkAuth , perfil);
+router.get('/perfil', checkAuth, perfil);
 router.put("/perfil/:id", checkAuth, actualizarPerfil);
 router.get("/lista-usuarios", checkAuth, usuarioRegistrados);
 router.put("/actualizar-password", checkAuth, actualizarPassword);
